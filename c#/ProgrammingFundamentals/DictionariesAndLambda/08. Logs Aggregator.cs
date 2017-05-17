ad5e290b065e1428bb75a2329135f016abddffa5
i-den <idenchev07@gmail.com>
2017-05-17 21:07:23 +0300

    Added several other Dictionaries Exercises

---
+++ c#/ProgrammingFundamentals/DictionariesAndLambda/08. Logs Aggregator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        var userSessionIPList = new Dictionary<string, Dictionary<string, int>>();
+        var numberOfRequests = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfRequests; i++)
+        {
+            var input = Console.ReadLine().Split();
+
+            var currentUserName = input[1];
+            var currentUserIP = input[0];
+            var currentUserSessionTime = int.Parse(input[2]);
+
+            if (!userSessionIPList.ContainsKey(currentUserName))
+            {
+                userSessionIPList[currentUserName] = new Dictionary<string, int>();
+            }
+
+            if (!userSessionIPList[currentUserName].ContainsKey(currentUserIP))
+            {
+                userSessionIPList[currentUserName][currentUserIP] = 0;
+            }
+
+            userSessionIPList[currentUserName][currentUserIP] += currentUserSessionTime;
+        }
+
+        foreach (var userIPSessionData in userSessionIPList.OrderBy(x => x.Key))
+        {
+            var currUserName = userIPSessionData.Key;
+            var currUserIPData = userIPSessionData.Value;
+
+            var currentTotalSessionTime = userIPSessionData.Value.Values.Sum();
+            var totalIPs = userIPSessionData.Value.Keys.Count();
+            var counter = 1;
+
+            Console.Write($"{currUserName}: {currentTotalSessionTime} [");
+
+            foreach (var IPs in currUserIPData.OrderBy(x => x.Key))
+            {
+                var currentIP = IPs.Key;
+
+                if (counter != totalIPs)
+                {
+                    Console.Write($"{currentIP}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentIP}]");
+                }
+
+                counter++;
+            }
+        }
+    }
+}
---
+++ c#/ProgrammingFundamentals/DictionariesAndLambda/09. Legendary Farming.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        var legendariesList = new Dictionary<string, int>()
+        {
+            {"motes", 0 },
+            {"shards", 0 },
+            {"fragments", 0 }
+        };
+        var junkList = new Dictionary<string, int>();
+        var canContinue = true;
+
+        while (canContinue)
+        {
+            var input = Console.ReadLine().Split();
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                var currentResourceQuantity = int.Parse(input[i]);
+                var currentResourceType = input[i + 1].ToLower();
+
+                if (legendariesList.ContainsKey(currentResourceType))
+                {
+                    legendariesList[currentResourceType] += currentResourceQuantity;
+
+                    if (legendariesList[currentResourceType] >= 250)
+                    {
+                        legendariesList[currentResourceType] -= 250;
+                        var legendaryItemObtained = string.Empty;
+
+                        switch (currentResourceType)
+                        {
+                            case "motes": legendaryItemObtained = "Dragonwrath"; break;
+                            case "shards": legendaryItemObtained = "Shadowmourne"; break;
+                            case "fragments": legendaryItemObtained = "Valanyr"; break;
+                        }
+
+                        Console.WriteLine($"{legendaryItemObtained} obtained!");
+
+                        canContinue = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (!junkList.ContainsKey(currentResourceType))
+                    {
+                        junkList[currentResourceType] = 0;
+                    }
+
+                    junkList[currentResourceType] += currentResourceQuantity;
+                }
+            }
+        }
+
+        foreach (var legendaryItem in legendariesList.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            var legendaryItemName = legendaryItem.Key;
+            var legendaryMatsQuantity = legendaryItem.Value;
+
+            Console.WriteLine($"{legendaryItemName}: {legendaryMatsQuantity}");
+        }
+
+        foreach (var junkItem in junkList.OrderBy(x => x.Key))
+        {
+            var junkItemName = junkItem.Key;
+            var junkMatsQuantity = junkItem.Value;
+
+            Console.WriteLine($"{junkItemName}: {junkMatsQuantity}");
+        }
+    }
+}
