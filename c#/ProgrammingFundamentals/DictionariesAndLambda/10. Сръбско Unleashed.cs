c7f1e837d7ee8ceb23460692f39cf12abb63d308
i-den <idenchev07@gmail.com>
2017-05-18 21:17:04 +0300

    Added final Dictionaries Exercies

---
+++ c#/ProgrammingFundamentals/DictionariesAndLambda/10. Сръбско Unleashed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    static void Main()
+    {
+        var singerVenueList = new Dictionary<string, Dictionary<string, int>>();
+        var canContinue = true;
+
+        while (canContinue)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "End")
+            {
+                canContinue = false;
+                break;
+            }
+
+            string correctInputPattern = @"(([a-zA-Z]+\s){1,3})@(([a-zA-Z]+\s){1,3})(\d+)\s(\d+)";
+
+            if (Regex.IsMatch(input, correctInputPattern))
+            {
+                Match match = Regex.Match(input, correctInputPattern);
+
+                string singer = match.Groups[1].Value.Trim();
+                string venue = match.Groups[3].Value.Trim();
+                int ticketsPrice = int.Parse(match.Groups[5].Value);
+                int ticketsCount = int.Parse(match.Groups[6].Value);
+
+                int totalMoney = ticketsCount * ticketsPrice;
+
+                if (!singerVenueList.ContainsKey(venue))
+                {
+                    singerVenueList[venue] = new Dictionary<string, int>();
+                }
+
+                if (!singerVenueList[venue].ContainsKey(singer))
+                {
+                    singerVenueList[venue][singer] = 0;
+                }
+
+                singerVenueList[venue][singer] += totalMoney;
+            }
+        }
+
+        foreach (var venueSingerPair in singerVenueList)
+        {
+            var currentVenue = venueSingerPair.Key;
+            var currentVenueData = venueSingerPair.Value;
+
+            Console.WriteLine(currentVenue);
+
+            foreach (var singerMoneyPair in currentVenueData.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine("#  {0} -> {1}", singerMoneyPair.Key, singerMoneyPair.Value);
+            }
+        }
+    }
+}
---
+++ c#/ProgrammingFundamentals/DictionariesAndLambda/11. Dragon Army.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        var dragonList = new Dictionary<string, SortedDictionary<string, decimal[]>>();
+        var totalDragons = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= totalDragons; i++)
+        {
+            var dragonInputInfo = Console.ReadLine().Split();
+
+            var currentDragonType = dragonInputInfo[0];
+            var currentDragonName = dragonInputInfo[1];
+            var currentDragonDamage = dragonInputInfo[2] == "null" ? 45 : decimal.Parse(dragonInputInfo[2]);
+            var currentDragonHealth = dragonInputInfo[3] == "null" ? 250 : decimal.Parse(dragonInputInfo[3]);
+            var currentDragonArmor = dragonInputInfo[4] == "null" ? 10 : decimal.Parse(dragonInputInfo[4]);
+
+            if (!dragonList.ContainsKey(currentDragonType))
+            {
+                dragonList[currentDragonType] = new SortedDictionary<string, decimal[]>();
+            }
+            
+                dragonList[currentDragonType][currentDragonName] = new decimal[] { currentDragonDamage, currentDragonHealth, currentDragonArmor };
+        }
+
+        foreach (var dragonTypeInfo in dragonList)
+        {
+            var currentDragonType = dragonTypeInfo.Key;
+            var currentTypeDragons = dragonTypeInfo.Value;
+
+            var currentDragonTypeAverageDamage = currentTypeDragons.Values.Average(x => x[0]);
+            var currentDragonTypeAverageHealth = currentTypeDragons.Values.Average(x => x[1]);
+            var currentDragonTypeAverageArmor = currentTypeDragons.Values.Average(x => x[2]);
+
+            Console.WriteLine($"{currentDragonType}::({currentDragonTypeAverageDamage:F2}/{currentDragonTypeAverageHealth:F2}/{currentDragonTypeAverageArmor:F2})");
+
+            foreach (var dragonNameStatsPair in currentTypeDragons)
+            {
+                var currentDragonName = dragonNameStatsPair.Key;
+                var currentDragonDamage = dragonNameStatsPair.Value[0];
+                var currentDragonHealth = dragonNameStatsPair.Value[1];
+                var currentDragonArmor = dragonNameStatsPair.Value[2];
+
+                Console.WriteLine($"-{currentDragonName} -> damage: {currentDragonDamage}, health: {currentDragonHealth}, armor: {currentDragonArmor}");
+            }
+        }
+    }
+}
