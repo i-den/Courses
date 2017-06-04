1f81e2c4185307823d0d88ba886df99e7d5aca66
i-den <idenchev07@gmail.com>
2017-06-04 15:57:13 +0300

    Added Last 2 Problems - 14 & 15

---
+++ c#/ProgrammingFundamentals/12) Data Types and Variables - More Exercises/14. Boat Simulator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var firstBoat = char.Parse(Console.ReadLine());
+        var secondBoat = char.Parse(Console.ReadLine());
+        var stringsCount = int.Parse(Console.ReadLine());
+
+        var firstBoatMoves = 0;
+        var secondBoatMoves = 0;
+        var currentIteration = 1;
+        var canContinue = true;
+
+        while (currentIteration <= stringsCount && canContinue)
+        {
+            var currentInput = Console.ReadLine();
+            var currentMovesToAdvance = currentInput.Length;
+
+            if (currentInput == "UPGRADE")
+            {
+                firstBoat = (char)(firstBoat + 3);
+                secondBoat = (char)(secondBoat + 3);
+            }
+            else if (currentIteration % 2 != 0)
+            {
+                CalculateBoatAdvance(firstBoat, ref firstBoatMoves, currentMovesToAdvance, ref canContinue);
+            }
+            else
+            {
+                CalculateBoatAdvance(secondBoat, ref secondBoatMoves, currentMovesToAdvance, ref canContinue);
+            }
+
+            currentIteration++;
+        }
+
+        if (canContinue)
+        {
+            Console.WriteLine(firstBoatMoves > secondBoatMoves ? firstBoat : secondBoat);
+        }
+    }
+
+    public static void CalculateBoatAdvance(char currentBoat, ref int currentBoatMoves, int currentMovesToAdvance, ref bool canContinue)
+    {
+        currentBoatMoves += currentMovesToAdvance;
+
+        if (currentBoatMoves >= 50)
+        {
+            Console.WriteLine(currentBoat);
+            canContinue = false;
+        }
+    }
+}
---
+++ c#/ProgrammingFundamentals/12) Data Types and Variables - More Exercises/15. Balanced Brackets.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var linesCount = int.Parse(Console.ReadLine());
+
+        var openingBracesCount = 0;
+        var closingBracesCount = 0;
+        var previousOpeningBracesCount = 0;
+        var previousClosingBracesCount = 0;
+        var previousInput = string.Empty;
+
+        for (int i = 0; i < linesCount; i++)
+        {
+            var currentInput = Console.ReadLine();
+
+            if (currentInput == "(")
+            {
+                openingBracesCount++;
+
+                if (previousInput == string.Empty)
+                {
+                    previousInput = "(";
+                }
+
+                if (previousInput == "(")
+                {
+                    previousOpeningBracesCount++;
+                }
+                else if (previousInput == ")")
+                {
+                    if (previousOpeningBracesCount != previousClosingBracesCount)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                    else
+                    {
+                        previousInput = "(";
+                        previousOpeningBracesCount = 1;
+                        previousClosingBracesCount = 0;
+                    }
+                }
+            }
+            else if (currentInput == ")")
+            {
+                closingBracesCount++;
+
+                if (previousInput == string.Empty)
+                {
+                    Console.WriteLine("UNBALANCED");
+                    return;
+                }
+                else if (previousInput == "(")
+                {
+                    previousInput = ")";
+                    previousClosingBracesCount++;
+                }
+                else if (previousInput == ")")
+                {
+                    previousClosingBracesCount++;
+                }
+            }
+        }
+
+        if (openingBracesCount == closingBracesCount)
+        {
+            Console.WriteLine("BALANCED");
+        }
+        else
+        {
+            Console.WriteLine("UNBALANCED");
+        }
+    }
+}
