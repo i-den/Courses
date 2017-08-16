df5956f88178bf669acc64e713bbb2fbb9724aea
i-den <idenchev07@gmail.com>
2017-08-16 19:43:46 +0300

    Added Exam Prep 1 - Java

---
+++ 03) Software Technologies/27) EXAM PREPARATION I - TODO LIST/Java/src/main/java/todolist/bindingModel/TaskBindingModel.java
@@ -0,0 +1,28 @@
+package todolist.bindingModel;
+
+public class TaskBindingModel {
+
+    private String title;
+
+    private String comments;
+
+    public TaskBindingModel() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
