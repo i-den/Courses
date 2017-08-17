5d335ac25c65b3f4efbcf246b9d3fa1257e9d072
i-den <idenchev07@gmail.com>
2017-08-17 14:40:25 +0300

    Added Exam Prep 2 - c#

---
+++ 03) Software Technologies/28) EXAM PREPARATION II - KANBAN BOARD/c#/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    [ValidateInput(false)]
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+
+                return View(tasks);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new TeisterMaskDbContext())
+                {
+                    db.Tasks.Add(task);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var task = db.Tasks.Find(id);
+
+                if (task == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(task);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Task taskModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(taskModel);
+            }
+
+            using (var db = new TeisterMaskDbContext())
+            {
+                var task = db.Tasks.Find(id);
+
+                if (task == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                task.Title = taskModel.Title;
+                task.Status = taskModel.Status;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
