e4096e09e512a5dee242b4cfcaaad127e3e25d81
i-den <24753710+i-den@users.noreply.github.com>
2019-08-31 13:49:08 +0300

    Added Comments and Small Refactoring to Arithmetic's methods

--- Math and Programming/Java/com/denchevgod/math/Arithmetic.java
+++ Math and Programming/Java/com/denchevgod/math/Arithmetic.java
@@ -22,25 +22,71 @@ public class Arithmetic {
             }
         }
 
+        /**
+         * Calculates the Greatest Common Divisor of multiple numbers
+         * using Prime Factorization and multiplying their intersecting Primes
+         *
+         * @param   nums    the numbers to calc GCD for
+         * @return          the Greatest Common Divisor of nums
+         */
         static int greatestCommonDivisor(int... nums) {
             return calcCommonDivisorOrMultiple(UsingVennDiagToCalc.GCD, nums);
         }
 
+        /**
+         * Calculates the Least Common Multiple of multiple numbers
+         * using Prime Factorization and multiplying the all of their Primes
+         * to the power of least occurrence
+         *
+         * @param   nums    the numbers to calc LCM for
+         * @return          the Least Common Multiple of nums
+         */
         static int leastCommonMultiple(int... nums) {
             return calcCommonDivisorOrMultiple(UsingVennDiagToCalc.LCM, nums);
         }
 
         static int calcCommonDivisorOrMultiple(UsingVennDiagToCalc vennDiagToCalc, int... nums) {
             List<List<Integer>> allNumsPrimeFact = Primes.multipleNumsPrimeFactorization(nums);
-            Map<Integer, Integer> primeNumsToPow = getIntersectingNumOccurrences(allNumsPrimeFact, vennDiagToCalc.val);
+            Map<Integer, Integer> primeNumsToPow = getIntersectingNumOccurrences(vennDiagToCalc.val, allNumsPrimeFact);
             return primeNumsToPow.keySet().stream()
                     .mapToInt(a -> (int) Math.pow(a, primeNumsToPow.get(a)))
                     .reduce((a, b) -> a * b)
-                    .orElse(1);
+                    .orElse(1); // 1 for GCD
         }
 
-        private static Map<Integer, Integer> getIntersectingNumOccurrences(List<List<Integer>> numsPrimeFact, boolean vennDiagram) {
-            List<Map<Integer, Integer>> allNumOccurrences = new ArrayList<>();
+
+        /**
+         * Returns a Map<Integer, Integer> representing Prime Numbers
+         * used to calc LCM or GCD and the number of their Occurrences (power)
+         * taken from their Prime Factorization
+         *
+         * GCD uses the Common Intersection Primes from the Prime Factorization of all numbers
+         * (https://en.wikipedia.org/wiki/Greatest_common_divisor)
+         *
+         * LCM uses all Primes in the Prime Factorization to the power of least occurrences
+         * (https://en.wikipedia.org/wiki/Least_common_multiple)
+         *
+         * Examples:
+         * Greatest Common Divisor, vennDiagram == true
+         * getIntersectingNumOccurrences(true, {
+         *   { 2:1, 3:3, 5:4 },
+         *   { 2:2, 5:2, 7:3 }
+         * })                     // { 2:1, 5:2 }
+         *
+         *
+         * Least Common Multiple, vennDiagram == false
+         * getIntersectingNumOccurrences(true, {
+         *   { 2:1, 3:3, 5:4 },
+         *   { 2:2, 5:2, 7:3 }
+         * })                     // { 2:1, 3:3, 5:4, 7:3 }
+         *
+         *
+         * @param   vennDiagram     Whether the intersection of the Prime Factorization should be used or all Primes
+         * @param   numsPrimeFact   A list containing the Prime Factorization for each number that GCD or LCM is calculated for
+         * @return                  A map of all Primes and their Power that should be used to calculate GCD or LCM
+         */
+        private static Map<Integer, Integer> getIntersectingNumOccurrences(boolean vennDiagram, List<List<Integer>> numsPrimeFact) {
+            List<Map<Integer, Integer>> allNumOccurrences = new ArrayList<>(); // All Primes in their factorization and their number of occurrences (power)
 
             for (List<Integer> integers : numsPrimeFact) {
                 Map<Integer, Integer> numOccurrences = new HashMap<>();
@@ -50,7 +96,9 @@ public class Arithmetic {
                 }
                 allNumOccurrences.add(numOccurrences);
             }
-            Set<Integer> intersectingNums = allNumOccurrences.stream()
+
+            // Used for GCD, gets all Primes that are present in EVERY number's Prime Factorization
+            Set<Integer> primesInAllPrimeFacts = allNumOccurrences.stream()
                     .reduce((a, b) -> {
                         a.keySet().retainAll(b.keySet());
                         return a;
@@ -59,20 +107,20 @@ public class Arithmetic {
                     .orElse(allNumOccurrences.get(0).keySet());
 
             Map<Integer, Integer> intersectingNumsToPow = new HashMap<>();
-            for (Map<Integer, Integer> numOccurMap : allNumOccurrences) {
-                for (Map.Entry<Integer, Integer> numOccur : numOccurMap.entrySet()) {
-                    int numKey = numOccur.getKey();
-                    if (vennDiagram && intersectingNums.contains(numKey)) { // GCD
-                        intersectingNumsToPow.putIfAbsent(numKey, Integer.MAX_VALUE);
+            for (Map<Integer, Integer> primPowMap : allNumOccurrences) { // Each Prime Factorization's Prime Power
+                for (Map.Entry<Integer, Integer> primPow : primPowMap.entrySet()) { // Each Prime to its Power
+                    int prime = primPow.getKey();
+                    if (vennDiagram && primesInAllPrimeFacts.contains(prime)) { // GCD
+                        intersectingNumsToPow.putIfAbsent(prime, Integer.MAX_VALUE);
                         intersectingNumsToPow.put(
-                                numKey,
-                                Math.min(intersectingNumsToPow.get(numKey), numOccurMap.get(numKey))
+                                prime,
+                                Math.min(intersectingNumsToPow.get(prime), primPowMap.get(prime))
                         );
                     } else { // LCM
-                        intersectingNumsToPow.putIfAbsent(numKey, Integer.MIN_VALUE);
+                        intersectingNumsToPow.putIfAbsent(prime, Integer.MIN_VALUE);
                         intersectingNumsToPow.put(
-                                numKey,
-                                Math.max(intersectingNumsToPow.get(numKey), numOccurMap.get(numKey))
+                                prime,
+                                Math.max(intersectingNumsToPow.get(prime), primPowMap.get(prime))
                         );
                     }
                 }
@@ -80,60 +128,61 @@ public class Arithmetic {
             return intersectingNumsToPow;
         }
 
-        private static class Primes {
+    }
 
-            /**
-             * Finds the Prime Factors of an int
-             *
-             * @param n num to factorize
-             * @return a List of integers - the prime factors of the num n
-             */
-            static List<Integer> primeFactorization(int n) {
-                if (n < 1) {
-                    throw new IllegalArgumentException("1 cannot be Prime Factorized");
-                }
+    private static class Primes {
 
-                List<Integer> primeFactors = new LinkedList<>();
-                int lastKnownPrimeFactor;
-                while (n != 1) {
-                    lastKnownPrimeFactor = findLeastPrimeFactor(n);
-                    primeFactors.add(lastKnownPrimeFactor);
-                    n /= lastKnownPrimeFactor;
-                }
-                return primeFactors;
+        /**
+         * Finds the Prime Factors of an int
+         *
+         * @param n num to factorize
+         * @return a List of integers - the prime factors of the num n
+         */
+        static List<Integer> primeFactorization(int n) {
+            if (n < 1) {
+                throw new IllegalArgumentException("1 cannot be Prime Factorized");
             }
 
-            /**
-             * If n is not a prime it can be factored into two factors - a & b
-             * n = a * b
-             * If both a & b are greater than the sqrt(n) then their product will be greater than n
-             * At least one of the factors is smaller or equal than the sqrt(n)
-             * If there are no factors that are less than or equal to sqrt(n) - n is a Prime
-             *
-             * @param n Number to check the smallest Prime Factor for
-             * @return Smallest Prime Factor
-             */
-            static int findLeastPrimeFactor(int n) { //
-                if (n % 2 == 0) { // Even - smallest Prime Factor is 2
-                    return 2;
-                }
+            List<Integer> primeFactors = new LinkedList<>();
+            int lastKnownPrimeFactor;
+            while (n != 1) {
+                lastKnownPrimeFactor = findLeastPrimeFactor(n);
+                primeFactors.add(lastKnownPrimeFactor);
+                n /= lastKnownPrimeFactor;
+            }
+            return primeFactors;
+        }
 
-                int maxPossibleFactor = (int) Math.sqrt(n);
-                for (int i = 3; i <= maxPossibleFactor; i += 2) {
-                    if (n % i == 0) {
-                        return i;
-                    }
-                }
-                return n; // n is Prime
+        /**
+         * If n is not a prime it can be factored into two factors - a & b
+         * n = a * b
+         * If both a & b are greater than the sqrt(n) then their product will be greater than n
+         * At least one of the factors is smaller or equal than the sqrt(n)
+         * If there are no factors that are less than or equal to sqrt(n) - n is a Prime
+         *
+         * @param n Number to check the smallest Prime Factor for
+         * @return Smallest Prime Factor
+         */
+        static int findLeastPrimeFactor(int n) { //
+            if (n % 2 == 0) { // Even - smallest Prime Factor is 2
+                return 2;
             }
 
-            static List<List<Integer>> multipleNumsPrimeFactorization(int... nums) {
-                List<List<Integer>> primeFactAllNums = new ArrayList<>();
-                for (int num : nums) {
-                    primeFactAllNums.add(Primes.primeFactorization(num));
+            int maxPossibleFactor = (int) Math.sqrt(n);
+            for (int i = 3; i <= maxPossibleFactor; i += 2) {
+                if (n % i == 0) {
+                    return i;
                 }
-                return primeFactAllNums;
             }
+            return n; // n is Prime
+        }
+
+        static List<List<Integer>> multipleNumsPrimeFactorization(int... nums) {
+            List<List<Integer>> primeFactAllNums = new ArrayList<>();
+            for (int num : nums) {
+                primeFactAllNums.add(Primes.primeFactorization(num));
+            }
+            return primeFactAllNums;
         }
     }
 }
