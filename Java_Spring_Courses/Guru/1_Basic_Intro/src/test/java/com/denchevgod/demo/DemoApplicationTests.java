7f57a2bd49d6636b9f8a4c651b30d57bf59b7e72
iVan <vankata@localhost.localdomain>
2019-11-29 13:09:08 +0200

    Added Basic Spring intro

---
+++ Java_Spring_Courses/Guru/1_Basic_Intro/src/test/java/com/denchevgod/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denchevgod.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
